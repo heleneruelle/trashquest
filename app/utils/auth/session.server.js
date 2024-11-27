ae95a950becd32bfdc50aecb2dcd2964c049d830
Hélène Ruelle <ruellehelene26@gmail.com>
2024-11-27 16:34:14 +0100

    fix lint

--- app/utils/auth/session.server.js
+++ app/utils/auth/session.server.js
@@ -3,9 +3,7 @@ import { createCookieSessionStorage } from '@remix-run/node';
 const sessionSecret = process.env.SESSION_SECRET;
 
 if (!sessionSecret) {
-  throw new Error(
-    "SESSION_SECRET must be included in ENV VAR."
-  );
+  throw new Error('SESSION_SECRET must be included in ENV VAR.');
 }
 
 const { getSession, commitSession, destroySession } =
@@ -14,7 +12,7 @@ const { getSession, commitSession, destroySession } =
       name: '_session',
       secure: process.env.NODE_ENV === 'production', // HTTPS only in prod
       secrets: [sessionSecret],
-      sameSite: 'lax', 
+      sameSite: 'lax',
       path: '/',
       httpOnly: true, // Available server side only
     },
