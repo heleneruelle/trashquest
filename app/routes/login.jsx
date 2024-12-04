3d5e9caba054dc2d9238d1ee221ac79125dbfaa5
Hélène Ruelle <ruellehelene26@gmail.com>
2024-12-04 20:42:37 +0100

    test i18n setup

--- app/routes/login.jsx
+++ app/routes/login.jsx
@@ -1,13 +1,16 @@
 import LoginForm from '../components/auth/LoginForm';
+import { useTranslation } from 'react-i18next';
 
 import { loginAction } from '../loaders/login';
 
 export { loginAction as action };
 
 const LoginPage = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <h1>Please login to access TrashQuest !</h1>
+      <h1>{t('welcome')}</h1>
       <LoginForm />
     </div>
   );
