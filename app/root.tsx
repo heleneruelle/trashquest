3f07167c9278f748b3046a36ff5fa5660254476e
Hélène Ruelle <ruellehelene26@gmail.com>
2024-12-04 20:41:58 +0100

    add lang detection server side + i18n provider client side

--- app/root.tsx
+++ app/root.tsx
@@ -5,8 +5,29 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import i18nServer from '../i18n.server';
+import { LoaderFunctionArgs } from '@remix-run/node';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../i18n';
+import { useLoaderData } from '@remix-run/react';
+
+type LoaderData = {
+  lang: string;
+};
+
+export let loader = async ({
+  request,
+}: LoaderFunctionArgs): Promise<LoaderData> => {
+  const url = new URL(request.url);
+  const lang = url.pathname.split('/')[1] || 'en';
+  i18nServer.changeLanguage(lang);
+  return { lang };
+};
 
 export default function App() {
+  const { lang } = useLoaderData<LoaderData>();
+  i18n.changeLanguage(lang);
+
   return (
     <html lang="en">
       <head>
@@ -16,7 +37,9 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <I18nextProvider i18n={i18n}>
+          <Outlet />
+        </I18nextProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
