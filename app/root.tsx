ff607d266171fe51734cf9a2adde75f96c17d8e1
Hélène Ruelle <ruellehelene26@gmail.com>
2024-12-05 14:30:16 +0100

    update root route with I18nextProvider and LanguageSwitcher

--- app/root.tsx
+++ app/root.tsx
@@ -5,30 +5,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
-import i18nServer from '../i18n.server';
-import { LoaderFunctionArgs } from '@remix-run/node';
 import { I18nextProvider } from 'react-i18next';
-import i18n from '../i18n';
-import { useLoaderData } from '@remix-run/react';
-import { DEFAULT_LANGUAGE } from './config';
-
-type LoaderData = {
-  lang: string;
-};
-
-export let loader = async ({
-  request,
-}: LoaderFunctionArgs): Promise<LoaderData> => {
-  const url = new URL(request.url);
-  const lang = url.pathname.split('/')[1] || DEFAULT_LANGUAGE;
-  i18nServer.changeLanguage(lang);
-  return { lang };
-};
+import i18n from '~/i18n';
+import LanguageSwitcher from '~/components/LanguageSwitch';
 
 export default function App() {
-  const { lang } = useLoaderData<LoaderData>();
-  i18n.changeLanguage(lang);
-
   return (
     <html lang="en">
       <head>
@@ -38,7 +19,10 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <I18nextProvider i18n={i18n}>
+        <I18nextProvider i18n={i18n} defaultNS={'translation'}>
+          <div>
+            <LanguageSwitcher />
+          </div>
           <Outlet />
         </I18nextProvider>
         <ScrollRestoration />
