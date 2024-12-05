d9757620fedac1e2c5841ea55c0278e9116a8654
Hélène Ruelle <ruellehelene26@gmail.com>
2024-12-05 16:02:20 +0100

    add config values in i18n

--- app/i18n.js
+++ app/i18n.js
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { SUPPORTED_LANGUAGES, DEFAULT_LANGUAGE } from './config';
 
 if (!i18n.isInitialized) {
   i18n
@@ -10,12 +11,11 @@ if (!i18n.isInitialized) {
     .use(initReactI18next)
     .init({
       backend: {
-        loadPath: '/locales/{{lng}}/{{ns}}.json', // Matches public folder structure
+        loadPath: '/locales/{{lng}}/{{ns}}.json',
       },
-      fallbackLng: 'en', // Default language
-      supportedLngs: ['en', 'fr'], // Only allow these languages
-      defaultNS: 'translation', // Default namespace
-      debug: true, // Debug for troubleshooting
+      fallbackLng: DEFAULT_LANGUAGE,
+      supportedLngs: SUPPORTED_LANGUAGES,
+      defaultNS: 'translation',
     });
 }
 
