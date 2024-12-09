eed96671cbaca835cf72ad0f42c7d72bee2cfa3a
Hélène Ruelle <ruellehelene26@gmail.com>
2024-12-09 18:11:34 +0100

    add layouts

---
+++ app/components/templates/TwoColumnsLayout.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function TwoColumnsLayout({
+  children,
+  leftColumnWidth,
+}: {
+  children: React.ReactNode;
+  leftColumnWidth?: string;
+}) {
+
+  const [leftChild, rightChild] = React.Children.map(
+    children,
+    (child) => child
+  );
+
+  return (
+    <div className="two-columns-layout--wrapper">
+      <div
+        className="column-layout__left"
+        style={{
+          minWidth: leftColumnWidth,
+          maxWidth: leftColumnWidth,
+        }}
+      >
+        {leftChild}
+      </div>
+      <div className="column-layout__right">{rightChild}</div>
+    </div>
+  );
+}
+
+export default TwoColumnsLayout;
