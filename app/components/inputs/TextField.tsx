006f8bd21604e9dea79dab4d2210b6ce46056700
Hélène Ruelle <ruellehelene26@gmail.com>
2025-01-16 17:27:22 +0100

    create TextField

---
+++ app/components/inputs/TextField.tsx
@@ -0,0 +1,39 @@
+interface TextFieldProps {
+  type: string;
+  name: string;
+  required?: boolean;
+  label: string;
+  placeholder?: string | undefined;
+  error?: boolean;
+  hint?: string | undefined;
+  autoComplete?: 'off' | 'on';
+}
+
+function TextField({
+  type,
+  name,
+  required = true,
+  label,
+  placeholder,
+  error = false,
+  hint,
+}: TextFieldProps) {
+  return (
+    <label
+      className={`text-input ${error && 'text-input_error'}`}
+      htmlFor={name}
+    >
+      {label}
+      <input
+        id={name}
+        type={type}
+        name={name}
+        required={required}
+        placeholder={placeholder}
+      />
+      {hint && <small className="input-hint">{hint}</small>}
+    </label>
+  );
+}
+
+export default TextField;
