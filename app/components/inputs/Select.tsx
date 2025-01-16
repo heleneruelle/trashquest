aa051deccbf0b607ef5de136498926eeb61dd43e
Hélène Ruelle <ruellehelene26@gmail.com>
2025-01-16 17:26:14 +0100

    create select component

---
+++ app/components/inputs/Select.tsx
@@ -0,0 +1,35 @@
+import React, { ChangeEventHandler } from 'react';
+
+interface Option {
+  id: string | number;
+  value: string;
+  name: string;
+}
+
+interface SelectProps {
+  options: Option[];
+  name: string;
+  id: string;
+  hint: string;
+  changeCallback?: ChangeEventHandler<HTMLSelectElement> | undefined;
+}
+
+const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
+  ({ options, name, id, hint, changeCallback }, ref) => {
+    return (
+      <div className="select-container">
+        <label htmlFor={id}>{name}</label>
+        <select name={id} id={id} ref={ref} onChange={changeCallback}>
+          {options.map((option) => (
+            <option value={option.value} key={option.id}>
+              {option.name}
+            </option>
+          ))}
+        </select>
+        {hint && <small className="input-hint">{hint}</small>}
+      </div>
+    );
+  }
+);
+
+export default Select;
