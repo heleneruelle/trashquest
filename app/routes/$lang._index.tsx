378da2121c0618831c786ac2f0184d94cfe87cda
Hélène Ruelle <ruellehelene26@gmail.com>
2025-01-16 17:46:22 +0100

    update main styling

--- app/routes/$lang._index.tsx
+++ app/routes/$lang._index.tsx
@@ -1,18 +1,21 @@
-import type { MetaFunction, LinksFunction } from '@remix-run/node';
-import { Form } from '@remix-run/react';
-import { logoutAction } from '~/loaders/logout';
+import type { LinksFunction } from '@remix-run/node';
 import { useTranslation } from 'react-i18next';
 import { LoaderFunctionArgs } from '@remix-run/node';
 import { getSession } from '~/utils/auth/session.server';
 import { redirect } from '@remix-run/node';
 import i18nServer from '../i18n.server';
 import createCompositeUrl from '~/utils/url/createCompositeUrl';
+import TwoColumnsLayout from '~/components/templates/TwoColumnsLayout';
+import ImageLayout from '~/components/templates/ImageLayout';
+import Map from '~/components/map/Map';
+import Main from '~/pages/Main';
 
 export const links: LinksFunction = () => {
-  return [{ rel: 'stylesheet', href: '/styles/main.css' }];
+  return [
+    { rel: 'stylesheet', href: '/styles/main.css' },
+    { rel: 'stylesheet', href: '/styles/form.css' },
+  ];
 };
-
-export { logoutAction as action };
 
 type LoaderData = {
   isAuthenticated: boolean;
@@ -20,15 +23,11 @@ type LoaderData = {
 };
 
 export let loader = async ({
-  params,
   request,
 }: LoaderFunctionArgs): Promise<LoaderData> => {
   // Session handle
   const session = await getSession(request.headers.get('Cookie'));
   const userId = session.get('userId');
-
-  const url = new URL(request.url);
-  const pathname = url.pathname;
 
   if (!userId) {
     throw redirect(createCompositeUrl(i18nServer, '/login'));
@@ -40,12 +39,9 @@ export default function Index() {
   const { t } = useTranslation();
 
   return (
-    <div className="welcome__container--wip">
-      <h1>{t('welcome')}</h1>
-      <p className="welcome__description">{t('description')}</p>
-      <Form method="post">
-        <button type="submit">{t('logout.cta')}</button>
-      </Form>
-    </div>
+    <TwoColumnsLayout>
+      <Main />
+      <ImageLayout />
+    </TwoColumnsLayout>
   );
 }
