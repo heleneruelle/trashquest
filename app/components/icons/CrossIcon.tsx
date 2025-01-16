4acf1284467160767002f57b3e17f4df71b6a792
Hélène Ruelle <ruellehelene26@gmail.com>
2025-01-16 17:45:03 +0100

    update LoginForm (style + use new comps)

---
+++ app/components/icons/CrossIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+
+const CloseIcon: React.FC = () => {
+  return <FaTimes size={24} />;
+};
+
+export default CloseIcon;
--- app/routes/$lang.login.tsx
+++ app/routes/$lang.login.tsx
@@ -1,6 +1,13 @@
+import { LinksFunction } from '@remix-run/node';
 import LoginPage from '~/pages/Login';
-
 import { loginAction } from '../loaders/login';
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: 'stylesheet', href: '/styles/form.css' },
+    { rel: 'stylesheet', href: '/styles/login.css' },
+  ];
+};
 
 export { loginAction as action };
 
