f86a6d71d14d767b721b1bcbc07d2d5ec09dab1d
Hélène Ruelle <ruellehelene26@gmail.com>
2025-01-18 18:27:59 +0100

    fix import

--- app/pages/SignUp.tsx
+++ app/pages/SignUp.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import SignUpForm from '~/components/auth/SignUpForm';
+import SignUpForm from '~/components/forms/SignUpForm';
 import ImageLayout from '~/components/templates/ImageLayout';
 
 const SignUpPage = () => {
