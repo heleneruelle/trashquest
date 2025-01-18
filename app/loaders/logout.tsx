52123b92ec9570fd9eaf5e0d28f317a45425d92b
Hélène Ruelle <ruellehelene26@gmail.com>
2025-01-18 19:21:10 +0100

    minimal setup test

--- app/loaders/logout.tsx
+++ app/loaders/logout.tsx
@@ -1,11 +1,11 @@
-import { ActionFunctionArgs } from '@remix-run/node';
+/* import { ActionFunctionArgs } from '@remix-run/node';
 import { getSession, destroySession } from '~/utils/auth/session.server';
-import signOut from '../utils/auth/signout';
+import signOut from '../utils/auth/signout'; */
 import { redirect } from '@remix-run/node';
-import i18nServer from '~/i18n.server';
-import createCompositeUrl from '~/utils/url/createCompositeUrl';
+/* import i18nServer from '~/i18n.server';
+import createCompositeUrl from '~/utils/url/createCompositeUrl'; */
 
-export async function logoutAction({ request }: ActionFunctionArgs) {
+/* export async function logoutAction({ request }: ActionFunctionArgs) {
   await signOut();
   const session = await getSession(request.headers.get('Cookie'));
   return redirect(createCompositeUrl(i18nServer, '/login'), {
@@ -13,4 +13,9 @@ export async function logoutAction({ request }: ActionFunctionArgs) {
       'Set-Cookie': await destroySession(session),
     },
   });
+}
+ */
+export async function logoutAction() {
+  console.log('logout action triggered');
+  return redirect('/');
 }
