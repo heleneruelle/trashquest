9a7b8f5de245e1924a0b97fe3d22b9802d919c82
Hélène Ruelle <ruellehelene26@gmail.com>
2025-01-18 22:11:41 +0100

    update LanguageSwitch = pathname persist

--- app/components/LanguageSwitch.tsx
+++ app/components/LanguageSwitch.tsx
@@ -1,15 +1,20 @@
-import { Link } from '@remix-run/react';
+import { Link, useLocation } from '@remix-run/react';
+import { useTranslation } from 'react-i18next';
 import createCompositeUrl from '~/utils/url/createCompositeUrl';
 import i18n from '~/i18n';
 import { SUPPORTED_LANGUAGES } from '~/config';
-import { useTranslation } from 'react-i18next';
 
 function LanguageSwitcher() {
   const { t } = useTranslation();
+  const { pathname } = useLocation();
+  const noLanguagePathname = pathname.replace(/^\/([a-zA-Z]{2})\//, '/');
   return (
     <div className="language-switch__container">
       {SUPPORTED_LANGUAGES.map((language) => (
-        <Link to={createCompositeUrl(i18n, '', { language })} key={language}>
+        <Link
+          to={createCompositeUrl(i18n, noLanguagePathname, { language })}
+          key={language}
+        >
           {t(language)}
         </Link>
       ))}
