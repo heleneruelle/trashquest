3f00ea616073c888fc1953be1a738e9beb6faeb7
Hélène Ruelle <ruellehelene26@gmail.com>
2025-01-19 01:21:23 +0100

    update questform

--- app/components/forms/QuestForm.tsx
+++ app/components/forms/QuestForm.tsx
@@ -22,7 +22,7 @@ function QuestForm() {
         name="name"
         label={t('create-new-quest.input.name')}
       />
-      <LocationAutoComplete />
+      <LocationAutoComplete hint={t('create-new-quest.hint.location')} types={['address']}/>
       <fieldset>
         <legend className="date-time">
           {t('create-new-quest.input.start')}
