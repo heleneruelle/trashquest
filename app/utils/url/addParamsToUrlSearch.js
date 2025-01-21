0cf206795214d274161a642f83db70bb99b97d10
Hélène Ruelle <ruellehelene26@gmail.com>
2025-01-21 12:26:25 +0100

    refactor addParamsToUrlSearch avoid dupe params

--- app/utils/url/addParamsToUrlSearch.js
+++ app/utils/url/addParamsToUrlSearch.js
@@ -1,5 +1,8 @@
 const addParamsToUrlSearch = (params, searchParams) => {
   for (const [key, value] of Object.entries(params)) {
+    if (searchParams.get(key)) {
+      searchParams.delete(key);
+    }
     if (value) {
       searchParams.append(key, value);
     }
