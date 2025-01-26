19894fe23d39786d1ac83c16b1d2db4f7ccd9e96
Hélène Ruelle <ruellehelene26@gmail.com>
2025-01-26 18:29:21 +0100

    stash

--- app/components/forms/LoginForm.jsx
+++ app/components/forms/LoginForm.jsx
@@ -1,13 +1,16 @@
 import { Form, useActionData, useNavigation } from '@remix-run/react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import TextField from '../inputs/TextField';
 import Button from '../inputs/Button';
 import Toast from '../notifications/Toast';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebaseConfig';
 
 const LoginForm = () => {
   const navigation = useNavigation();
   const actionData = useActionData();
+  const formRef = useRef(null);
 
   const { t } = useTranslation();
 
@@ -19,8 +22,35 @@ const LoginForm = () => {
     }
   }, [actionData]);
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!formRef.current) return;
+
+    const formData = new FormData(formRef.current); // Utilisation de FormData pour récupérer les valeurs des champs
+
+    // Récupérer les valeurs des inputs à partir de formData
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    if (!email || !password) {
+      setError('Veuillez remplir tous les champs');
+      return;
+    }
+
+    try {
+      // Appel à Firebase pour se connecter
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log('Utilisateur connecté');
+      // Rediriger ou effectuer d'autres actions après la connexion
+    } catch (err) {
+      setError('Erreur de connexion : ' + err.message);
+      console.error('Erreur de connexion:', err.message);
+    }
+  };
+
   return (
-    <Form method="post" className="form">
+    <Form ref={formRef} className="form" onSubmit={handleLogin}>
       {showError && (
         <Toast
           type="error"
