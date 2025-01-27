f7624a5108cc0d9f0a3b397ff976fd09baac6045
Hélène Ruelle <ruellehelene26@gmail.com>
2025-01-27 16:35:22 +0100

    update index with useAuth (redirect if no current session)

--- app/routes/$lang._index.tsx
+++ app/routes/$lang._index.tsx
@@ -1,13 +1,12 @@
 import type { LinksFunction } from '@remix-run/node';
-import { useTranslation } from 'react-i18next';
-import { LoaderFunctionArgs } from '@remix-run/node';
-/* import { getSession } from '~/utils/auth/session.server';
-import { redirect } from '@remix-run/node';
-import i18nServer from '../i18n.server';
-import createCompositeUrl from '~/utils/url/createCompositeUrl'; */
+import { useNavigate } from '@remix-run/react';
+import { useEffect } from 'react';
+import useAuth from '~/hooks/useAuth';
+import i18n from '~/i18n';
 import TwoColumnsLayout from '~/components/templates/TwoColumnsLayout';
 import ImageLayout from '~/components/templates/ImageLayout';
 import Main from '~/pages/Main';
+import createCompositeUrl from '~/utils/url/createCompositeUrl';
 
 export const links: LinksFunction = () => {
   return [
@@ -16,26 +15,15 @@ export const links: LinksFunction = () => {
   ];
 };
 
-type LoaderData = {
-  isAuthenticated: boolean;
-  //userId: string;
-};
-
-export let loader = async ({
-  request,
-}: LoaderFunctionArgs): Promise<LoaderData> => {
-  // Session handle
-  /* const session = await getSession(request.headers.get('Cookie'));
-  const userId = session.get('userId');
-
-  if (!userId) {
-    throw redirect(createCompositeUrl(i18nServer, '/login'));
-  } */
-  return { isAuthenticated: true };
-};
-
 export default function Index() {
-  const { t } = useTranslation();
+  const user = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate(createCompositeUrl(i18n, '/login'));
+    }
+  }, [user]);
 
   return (
     <TwoColumnsLayout>
