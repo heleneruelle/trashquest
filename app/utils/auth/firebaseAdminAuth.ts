46a150bb0d309a5a12e70ba949857697a87dd78a
Hélène Ruelle <ruellehelene26@gmail.com>
2025-01-30 16:06:03 +0100

    add firebase admin setup

---
+++ app/utils/auth/firebaseAdminAuth.ts
@@ -0,0 +1,22 @@
+import admin from 'firebase-admin';
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+      privateKey: process.env.VITE_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      clientEmail: process.env.VITE_FIREBASE_CLIENT_EMAIL,
+    }),
+  });
+}
+
+const verifyIdToken = async (idToken: string) => {
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    return decodedToken;
+  } catch (error) {
+    throw new Error('Token invalid or expired');
+  }
+};
+
+export { verifyIdToken };
