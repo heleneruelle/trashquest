b7b2fcf2ad01efc0b3462f013fe2d88770c5b68b
Hélène Ruelle <ruellehelene26@gmail.com>
2025-02-03 17:09:58 +0100

    Add styling

--- app/routes/$lang.quest.$id.tsx
+++ app/routes/$lang.quest.$id.tsx
@@ -1,5 +1,10 @@
+import { LinksFunction } from '@remix-run/node';
 import Quest from '~/pages/Quest';
 import questLoader from '~/loaders/quest';
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: '/styles/form.css' }];
+};
 
 export const loader = questLoader;
 
