87ac59a1630be3c634e6961174bf6e4d64994dda
Hélène Ruelle <ruellehelene26@gmail.com>
2025-02-03 17:09:27 +0100

    refactor questToVm with string to array

--- app/utils/tovm/questToVm.js
+++ app/utils/tovm/questToVm.js
@@ -1,10 +1,17 @@
 function questToVm(quest, creator) {
-  return {
+  const data = {
     ...quest,
+    properties: {
+      ...quest.properties,
+      equipment: quest.properties.equipment.split(','),
+      environment: quest.properties.environment.split(','),
+      accessibility: quest.properties.accessibility.split(','),
+    },
     creator,
     // TODO handle with current user
     isCurrentUserCreator: true,
   };
+  return data;
 }
 
 export default questToVm;
