14db2d6bc38c57174d7cafcff645ed9e3fe97eff
Hélène Ruelle <ruellehelene26@gmail.com>
2025-02-07 18:37:18 +0100

    Add join quest action + error

--- app/pages/Quest.tsx
+++ app/pages/Quest.tsx
@@ -1,13 +1,15 @@
-import { useLoaderData } from '@remix-run/react';
+import { useLoaderData, useParams } from '@remix-run/react';
+import { useState } from 'react';
 import TwoColumnsLayout from '~/components/templates/TwoColumnsLayout';
 import ImageLayout from '~/components/templates/ImageLayout';
 import Field from '~/components/display/Field';
 import Button from '~/components/inputs/Button';
 import FieldWithTag from '~/components/display/FieldWithTag';
 import ButtonLink from '~/components/inputs/ButtonLink';
+import Toast from '~/components/notifications/Toast';
 import createCompositeUrl from '~/utils/url/createCompositeUrl';
 import i18n from '~/i18n';
-import { auth } from '~/firebaseConfig';
+import useAuth from '~/hooks/useAuth';
 
 interface LoaderData {
   success: boolean;
@@ -25,6 +27,7 @@ interface LoaderData {
         days?: number;
         minutes?: number;
       };
+      isQuestFull: boolean;
     };
     location: {
       name: string;
@@ -38,57 +41,106 @@ interface LoaderData {
 
 function Quest() {
   const data = useLoaderData<LoaderData>();
+  const [error, setError] = useState(false);
 
-  if (data?.success && data?.quest) {
-    const { quest } = data || {};
-    const { properties, location, creator } = quest || {};
-    const isCurrentUserCreator = auth.currentUser?.uid === creator.uid;
+  if (!data?.success || !data?.quest) {
+    return <div>NO SUCH QUEST</div>;
+  }
 
-    return (
-      <TwoColumnsLayout>
-        <div>
-          <h1>{properties.name}</h1>
-          <p>{properties.description}</p>
-          <Field fieldName="location" fieldValue={location.name} />
-          <Field fieldName="organiser" fieldValue={creator.username} />
-          <Field
-            fieldName="duration"
-            fieldValue={JSON.stringify(properties.duration)}
+  const { id } = useParams();
+  const { user } = useAuth();
+
+  const { quest } = data || {};
+  const { properties, location, creator } = quest || {};
+
+  const isCurrentUserCreator = user?.uid === creator.uid;
+  const isCurrentUserQuestRegistered = properties.participants.includes(
+    user?.uid
+  );
+
+  async function handleJoinQuest(e: Event) {
+    e.preventDefault();
+    try {
+      if (!user) {
+        setError(true);
+      }
+      const idToken = await user.getIdToken();
+      const response = await fetch('/api/join-quest', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${idToken}`,
+        },
+        body: JSON.stringify({
+          id,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (data.error) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setError(true);
+    }
+  }
+
+  return (
+    <TwoColumnsLayout>
+      <div>
+        {error && (
+          <Toast
+            type="error"
+            message={'Join quest failed'}
+            callback={() => setError(false)}
           />
-          <Field
-            fieldName="participants"
-            fieldValue={`${properties.participants} / ${properties.expectedParticipants}`}
-          />
-          <FieldWithTag
-            fieldName="equipment"
-            fieldValues={properties.equipment}
-          />
-          <FieldWithTag
-            fieldName="environment"
-            fieldValues={properties.environment}
-          />
-          <FieldWithTag
-            fieldName="accessibility"
-            fieldValues={properties.accessibility}
-          />
-          {/* TODO handle join quest action */}
-          {!isCurrentUserCreator && (
-            <button disabled={true} type="button">
-              {'join this quest'}
-            </button>
-          )}
+        )}
+        <h1>{properties.name}</h1>
+        <p>{properties.description}</p>
+        <Field fieldName="location" fieldValue={location.name} />
+        <Field fieldName="organiser" fieldValue={creator.username} />
+        <Field
+          fieldName="duration"
+          fieldValue={JSON.stringify(properties.duration)}
+        />
+        <Field
+          fieldName="participants"
+          fieldValue={`${properties.participants.length} / ${properties.expectedParticipants}`}
+        />
+        <FieldWithTag
+          fieldName="equipment"
+          fieldValues={properties.equipment}
+        />
+        <FieldWithTag
+          fieldName="environment"
+          fieldValues={properties.environment}
+        />
+        <FieldWithTag
+          fieldName="accessibility"
+          fieldValues={properties.accessibility}
+        />
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '6px' }}>
+          {!isCurrentUserCreator &&
+            !properties.isQuestFull &&
+            !isCurrentUserQuestRegistered && (
+              <Button
+                type="button"
+                label="Join this quest"
+                clickCallback={handleJoinQuest}
+              />
+            )}
           <ButtonLink label="Go Home" target={createCompositeUrl(i18n, '/')} />
           <ButtonLink
             label="Create new quest"
             target={createCompositeUrl(i18n, '/create-new')}
           />
         </div>
-        <ImageLayout />
-      </TwoColumnsLayout>
-    );
-  }
-
-  return <div>NO SUCH QUEST</div>;
+      </div>
+      <ImageLayout />
+    </TwoColumnsLayout>
+  );
 }
 
 export default Quest;
