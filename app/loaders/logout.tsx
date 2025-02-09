6623b0cac965e597118578ad4999b7e6b04c7052
Hélène Ruelle <ruellehelene26@gmail.com>
2025-02-09 15:44:55 +0100

    logout loader test

--- app/loaders/logout.tsx
+++ app/loaders/logout.tsx
@@ -2,16 +2,20 @@ import { destroyCookie } from '~/utils/cookies';
 
 const logoutAction = async () => {
   try {
-    const cookieHeader = destroyCookie('firebase_token');
+    /*     const cookieHeader = destroyCookie('firebase_token');
 
     const headers = new Headers();
     headers.append(
       'Cache-Control',
       'no-store, no-cache, must-revalidate, proxy-revalidate'
     );
-    headers.append('Set-Cookie', cookieHeader);
+    headers.append('Set-Cookie', cookieHeader); */
 
-    return Response.json({ success: true }, { headers });
+    return new Response('Logout successful', {
+      headers: {
+        'Set-Cookie': destroyCookie('firebase_token'),
+      },
+    });
   } catch (error) {
     console.log('Error during logout:', error);
     return Response.json({ error: `${error}` }, { status: 500 });
