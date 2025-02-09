6b222f2c7ad72485c66ae1264a0ceffd4cd2051d
Hélène Ruelle <ruellehelene26@gmail.com>
2025-02-09 16:25:48 +0100

    use fetch instead of fetcher with api route

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -1,4 +1,3 @@
-import { useFetcher } from '@remix-run/react';
 import { useNavigate } from '@remix-run/react';
 import createCompositeUrl from '~/utils/url/createCompositeUrl';
 import i18n from '~/i18n';
@@ -6,11 +5,10 @@ import { auth } from '~/firebaseConfig';
 
 const LogoutButton = () => {
   const navigate = useNavigate();
-  const fetcher = useFetcher();
 
   const handleLogout = async () => {
     try {
-      await fetcher.submit({}, { method: 'post', action: '/logout' });
+      await fetch('/api/logout', { method: 'POST' });
       await auth.signOut();
       return navigate(createCompositeUrl(i18n, '/login'));
     } catch (error) {
