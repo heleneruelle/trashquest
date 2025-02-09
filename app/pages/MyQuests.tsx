97346a3ea2c6623f658e3b886210f621a6835d00
Hélène Ruelle <ruellehelene26@gmail.com>
2025-02-09 21:24:07 +0100

    add display to MyQuests page

--- app/pages/MyQuests.tsx
+++ app/pages/MyQuests.tsx
@@ -1,5 +1,30 @@
+import { useLoaderData } from '@remix-run/react';
+import { useTranslation } from 'react-i18next';
+import QuestListItem from '~/components/display/QuestListItem';
+import QuestType from '~/types/quest';
+
+interface LoaderData {
+  success: boolean;
+  quests: Array<QuestType>;
+}
+
 function MyQuests() {
-  return <div>MyQuests</div>;
+  const { success, quests } = useLoaderData<LoaderData>();
+  const { t } = useTranslation();
+  return (
+    <div>
+      <h1>{t('my-quests.title')}</h1>
+      <ul>
+        {success && quests?.length
+          ? quests.map((quest) => (
+              <li key={quest.id}>
+                <QuestListItem quest={quest} />
+              </li>
+            ))
+          : null}
+      </ul>
+    </div>
+  );
 }
 
 export default MyQuests;
