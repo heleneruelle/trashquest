d45882d633f84fc24f07eeab684aa12695bccea7
Hélène Ruelle <ruellehelene26@gmail.com>
2025-02-09 15:26:48 +0100

    fetch logout action on LogoutButton clicked

--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -1,17 +1,20 @@
+import { useFetcher } from '@remix-run/react';
 import { useNavigate } from '@remix-run/react';
-import { auth } from '~/firebaseConfig';
 import createCompositeUrl from '~/utils/url/createCompositeUrl';
 import i18n from '~/i18n';
+import { auth } from '~/firebaseConfig';
 
 const LogoutButton = () => {
   const navigate = useNavigate();
+  const fetcher = useFetcher();
 
   const handleLogout = async () => {
     try {
+      await fetcher.submit({}, { method: 'post', action: '/logout' });
       await auth.signOut();
-      navigate(createCompositeUrl(i18n, '/login'));
+      return navigate(createCompositeUrl(i18n, '/login'));
     } catch (error) {
-      console.error('Error logging out:', error);
+      console.log('Error during logout:', error);
     }
   };
 
