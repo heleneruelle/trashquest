be79d762e63da65efca0dcb26277c2fe0af05c8a
Hélène Ruelle <ruellehelene26@gmail.com>
2025-02-09 21:23:32 +0100

    create myquests loader (fetch all for current user)

--- app/loaders/myquest.tsx
+++ app/loaders/myquest.tsx
@@ -1,7 +1,45 @@
 import { LoaderFunctionArgs } from '@remix-run/node';
+import { getCookie } from '~/utils/cookies';
+import { verifyIdToken } from '~/utils/auth/firebaseAdminAuth';
+import { db } from '~/utils/auth/firebaseAdminAuth';
 
 async function myQuestsLoader({ request }: LoaderFunctionArgs) {
-  return null;
+  try {
+    const token = await getCookie(request, 'firebase_token');
+
+    if (!token) {
+      return Response.json({ error: 'No token provided' }, { status: 401 });
+    }
+    const decodedToken = await verifyIdToken(token);
+
+    const questsRef = db.collection('quests');
+    const query = questsRef.where(
+      'properties.creatorId',
+      '==',
+      decodedToken.uid
+    );
+
+    const data = await query
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          throw new Error(`No document found for ${decodedToken.uid}`);
+        }
+        const quests = [];
+        snapshot.forEach((doc) => {
+          quests.push(doc.data());
+        });
+
+        return quests;
+      })
+      .catch((err) => {
+        throw new Error(`Error while getting data from DB: ${err}`);
+      });
+
+    return Response.json({ success: true, quests: data }, { status: 200 });
+  } catch (error) {
+    return Response.json({ error: `${error}` }, { status: 500 });
+  }
 }
 
 export default myQuestsLoader;
