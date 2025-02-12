e8176dc0e388d4c1634e89f3e321fe809ea0c169
Hélène Ruelle <ruellehelene26@gmail.com>
2025-02-12 21:16:23 +0100

    create findClosestQuest utils

---
+++ app/utils/quests/findClosestQuest.js
@@ -0,0 +1,29 @@
+import { point, distance } from '@turf/turf';
+
+async function findClosestQuest({ lat, lon }, quests) {
+  try {
+    const closestQuest = quests.reduce((closestQuest, quest) => {
+      const questLat = quest.location.coordinates.latitude;
+      const questLon = quest.location.coordinates.longitude;
+
+      const userPoint = point([lon, lat]);
+
+      const questPoint = point([questLon, questLat]);
+
+      const dist = distance(userPoint, questPoint, {
+        units: 'kilometers',
+      });
+
+      if (!closestQuest || dist < closestQuest.dist) {
+        return { ...quest, dist };
+      }
+
+      return closestQuest;
+    }, null);
+    return closestQuest;
+  } catch (err) {
+    throw new Error(`Error while parsing closest quest : ${err}`);
+  }
+}
+
+export default findClosestQuest;
