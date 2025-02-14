0620a01abc17ae05e75785c56432067bbc0292ce
Hélène Ruelle <ruellehelene26@gmail.com>
2025-02-14 15:49:40 +0100

    display current user position on map

--- app/components/map/Map.tsx
+++ app/components/map/Map.tsx
@@ -1,17 +1,51 @@
-import Map from 'react-map-gl/mapbox';
+import { useMatches } from '@remix-run/react';
+import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import Map, { Marker } from 'react-map-gl/mapbox';
+import Pin from '../display/Pin';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 function MapComp() {
+  const [root] = useMatches();
+  const { t } = useTranslation();
+
+  const { data } = root;
+  const { longitude, latitude } = useMemo(() => {
+    if (
+      data?.user?.location?.coordinates?._longitude &&
+      data?.user?.location?.coordinates?._latitude
+    ) {
+      return {
+        longitude: data?.user?.location?.coordinates._longitude,
+        latitude: data?.user?.location?.coordinates._latitude,
+      };
+    } else return {};
+  }, [data]);
+
   return (
     <Map
       mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
       initialViewState={{
-        longitude: 2.285358886316118,
-        latitude: 46.71832756395037,
-        zoom: 4.772664089144272,
+        longitude: longitude || 2.285358886316118,
+        latitude: latitude || 46.71832756395037,
+        zoom: 11,
       }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    >
+      {data?.user?.location?.coordinates ? (
+        <Marker
+          key={`user-marker`}
+          longitude={data?.user?.location?.coordinates._longitude}
+          latitude={data?.user?.location?.coordinates._latitude}
+          anchor="bottom"
+        >
+          <div className="user-marker__tooltip">
+            <p>{t('map.user-position')}</p>
+          </div>
+          <Pin />
+        </Marker>
+      ) : null}
+    </Map>
   );
 }
 
