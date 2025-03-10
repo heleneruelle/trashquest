0a96a604f1d1fc9701326559b48e1f5539e97b72
Hélène Ruelle <ruellehelene26@gmail.com>
2025-03-10 12:17:17 +0100

    create MultiSelectInput comp

---
+++ app/components/display/MultiSelectInput.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSearchParams } from '@remix-run/react';
+import Select from 'react-select';
+
+type Option = {
+  value: string;
+  label: string;
+};
+
+function getOptionsFromSearchParams(
+  param: string | null,
+  defaultOptions: Option[]
+) {
+  const parsedParam = !param || param === '' ? [] : param.split(',');
+  return (
+    parsedParam?.map((e) => defaultOptions.find((opt) => opt.value === e)) || []
+  );
+}
+
+function getTranslatedLabelsForOptions(options, t) {
+  return options.map((opt) => ({ ...opt, label: t(opt.label) }));
+}
+
+function MultiSelectInput({
+  id,
+  defaultOptions,
+  placeholder,
+  label,
+}: {
+  id: string;
+  defaultOptions: Option[];
+  placeholder: string;
+  label: string;
+}) {
+  const [searchParams] = useSearchParams();
+  const { t } = useTranslation();
+  const searchParamForId = searchParams.get(id);
+
+  const [selectedOptions, setSelectedOptions] = useState(
+    getOptionsFromSearchParams(searchParamForId, defaultOptions)
+  );
+
+  return (
+    <label className="multi-select-input">
+      <strong>{label}</strong>
+      <Select
+        defaultValue={getTranslatedLabelsForOptions(selectedOptions, t)}
+        onChange={setSelectedOptions}
+        options={getTranslatedLabelsForOptions(defaultOptions, t)}
+        isMulti
+        id={id}
+        placeholder={placeholder}
+      />
+      <input
+        type="hidden"
+        name={id}
+        value={selectedOptions.map((e) => e.value)}
+      />
+    </label>
+  );
+}
+
+export default MultiSelectInput;
