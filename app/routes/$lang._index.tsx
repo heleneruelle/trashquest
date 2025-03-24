2c73083b8079efeadfd8f6cbf60ea3c61e853a9f
Hélène Ruelle <ruellehelene26@gmail.com>
2025-03-24 14:59:00 +0100

    reverse rendering for /home and _index routes

--- app/routes/$lang._index.tsx
+++ app/routes/$lang._index.tsx
@@ -1,36 +1,11 @@
-import type { LinksFunction } from '@remix-run/node';
-import { useNavigate } from '@remix-run/react';
-import { useEffect } from 'react';
-import questsLoader from '~/loaders/quests';
-import Main from '~/pages/Main';
-import Loading from '~/pages/Loading';
-import createCompositeUrl from '~/utils/url/createCompositeUrl';
-import useAuth from '~/hooks/useAuth';
-import i18n from '~/i18n';
+import { LinksFunction } from '@remix-run/node';
+import Welcome from '~/pages/Welcome';
 
 export const links: LinksFunction = () => {
   return [
-    { rel: 'stylesheet', href: '/styles/main.css' },
     { rel: 'stylesheet', href: '/styles/form.css' },
-    { rel: 'stylesheet', href: '/styles/quests.css' },
+    { rel: 'stylesheet', href: '/styles/welcome.css' },
   ];
 };
 
-export const loader = questsLoader;
-
-export default function Index() {
-  const { user, loading } = useAuth();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!user && !loading) {
-      navigate(createCompositeUrl(i18n, '/login'));
-    }
-  }, [user, loading, navigate]);
-
-  if (user && !loading) {
-    return <Main />;
-  }
-
-  return <Loading />;
-}
+export default Welcome;
--- app/routes/$lang.home.tsx
+++ app/routes/$lang.home.tsx
@@ -1,11 +1,36 @@
-import { LinksFunction } from '@remix-run/node';
-import Home from '~/pages/Home';
+import type { LinksFunction } from '@remix-run/node';
+import { useNavigate } from '@remix-run/react';
+import { useEffect } from 'react';
+import questsLoader from '~/loaders/quests';
+import Main from '~/pages/Main';
+import Loading from '~/pages/Loading';
+import createCompositeUrl from '~/utils/url/createCompositeUrl';
+import useAuth from '~/hooks/useAuth';
+import i18n from '~/i18n';
 
 export const links: LinksFunction = () => {
   return [
+    { rel: 'stylesheet', href: '/styles/main.css' },
     { rel: 'stylesheet', href: '/styles/form.css' },
-    { rel: 'stylesheet', href: '/styles/home.css' },
+    { rel: 'stylesheet', href: '/styles/quests.css' },
   ];
 };
 
-export default Home;
+export const loader = questsLoader;
+
+export default function Index() {
+  const { user, loading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user && !loading) {
+      navigate(createCompositeUrl(i18n, '/'));
+    }
+  }, [user, loading, navigate]);
+
+  if (user && !loading) {
+    return <Main />;
+  }
+
+  return <Loading />;
+}
