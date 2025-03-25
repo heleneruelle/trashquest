68c3cce32a7b7a5d85c2dd4706db98675b3afdbd
Hélène Ruelle <ruellehelene26@gmail.com>
2025-03-25 14:40:02 +0100

    display banner in QuestListItem v1

--- app/components/display/QuestListItem.tsx
+++ app/components/display/QuestListItem.tsx
@@ -58,6 +58,7 @@ function QuestListItem({ quest }: QuestListItemType) {
       to={createCompositeUrl(i18n, `/quest/${id}`)}
       className="quest-list-item"
     >
+      <img src={quest.bannerUrl} style={{ height: '100px', width: '100%' }} />
       <div className="quest-list-item__container">
         <div className="quest-list-item__title">
           <h4>{properties.name}</h4>
