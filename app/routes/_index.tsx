cd3bda95a998353d111ebf43ece2afcadeeae304
Hélène Ruelle <ruellehelene26@gmail.com>
2025-03-25 10:51:18 +0100

    remove meta from subroutes (added to root)

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,4 @@
 import type {
-  MetaFunction,
   LinksFunction,
   LoaderFunction,
 } from '@remix-run/node';
@@ -7,17 +6,6 @@ import { redirect } from '@remix-run/node';
 import { DEFAULT_LANGUAGE } from '~/config';
 import i18nServer from '~/i18n.server';
 import createCompositeUrl from '~/utils/url/createCompositeUrl';
-
-export const meta: MetaFunction = () => {
-  return [
-    { title: 'TrashQuest' },
-    {
-      name: 'description',
-      content:
-        'TrashQuest connects communities to clean up public spaces and protect nature!',
-    },
-  ];
-};
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: '/styles/main.css' }];
