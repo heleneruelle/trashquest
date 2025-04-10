2c1d4a892ad3c76d6ad79afd586943c01fa5f0d7
Hélène Ruelle <ruellehelene26@gmail.com>
2025-04-10 11:32:23 +0200

    add Quest list in user page

--- app/pages/User.tsx
+++ app/pages/User.tsx
@@ -1,20 +1,23 @@
 import { useLoaderData } from '@remix-run/react';
 import { useTranslation } from 'react-i18next';
 import UserType from '~/types/user';
+import QuestType from '~/types/quest';
+import QuestListItem from '~/components/display/QuestListItem';
 
 interface LoaderData {
   success: boolean;
   user: UserType;
+  quests: Array<QuestType>;
 }
 
 function User() {
-  const { user } = useLoaderData<LoaderData>();
+  const { user, quests } = useLoaderData<LoaderData>();
   const { t } = useTranslation();
 
   const { country, username } = user || {};
 
   return (
-    <div>
+    <div className="quests-container">
       <div style={{ display: 'flex' }}>
         <img
           src="/assets/default-avatar.webp"
@@ -23,6 +26,22 @@ function User() {
         <h1>{username}</h1>
       </div>
       <p>{t(`countries.${country}`)}</p>
+      <div>
+        {quests?.length ? (
+          <div>
+            <h4>{`Les quêtes organisées par ${username}`}</h4>
+            <ul className="quests-ul">
+              {quests.map((quest) => (
+                <li key={quest.id}>
+                  <QuestListItem quest={quest} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <h4>{`${username} n'organise pas de quête pour le moment`}</h4>
+        )}
+      </div>
     </div>
   );
 }
--- app/routes/$lang.user.$id.tsx
+++ app/routes/$lang.user.$id.tsx
@@ -1,5 +1,13 @@
+import { LinksFunction } from '@remix-run/node';
 import User from '~/pages/User';
 import userLoader from '~/loaders/user';
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: 'stylesheet', href: '/styles/form.css' },
+    { rel: 'stylesheet', href: '/styles/quests.css' },
+  ];
+};
 
 export const loader = userLoader;
 
