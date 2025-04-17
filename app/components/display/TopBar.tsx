dbb76b5a2e5cc7ea6061b4e535b3d43cc17b1514
Hélène Ruelle <ruellehelene26@gmail.com>
2025-04-17 17:16:03 +0200

    refactor topbar for mobile (add floating menu)

--- app/components/display/TopBar.tsx
+++ app/components/display/TopBar.tsx
@@ -1,15 +1,21 @@
 import { Link, NavLink } from '@remix-run/react';
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 import LanguageSwitcher from '../LanguageSwitch';
 import LogoutButton from '../LogoutButton';
 import { TbCircleLetterTFilled } from 'react-icons/tb';
+import { IoMenu } from 'react-icons/io5';
+import AuthenticatedNav from './AuthenticatedNav';
 import useAuth from '~/hooks/useAuth';
+import useIsMobile from '~/hooks/useIsMobile';
 import createCompositeUrl from '~/utils/url/createCompositeUrl';
 import i18n from '~/i18n';
 
 function TopBar() {
   const { user } = useAuth();
   const { t } = useTranslation();
+  const isMobile = useIsMobile();
+  const [openMenu, setOpenMenu] = useState(false);
 
   return (
     <div className="top-bar">
@@ -18,39 +24,38 @@ function TopBar() {
         to={createCompositeUrl(i18n, user ? '/home' : '/')}
       >
         <TbCircleLetterTFilled size={30} />
-        <span>Trashquest</span>
+        {isMobile ? null : <span>Trashquest</span>}
       </Link>
       <div className="top-bar__right">
-        <div className="top-bar__nav">
-          {user && (
-            <NavLink to={createCompositeUrl(i18n, '/home')}>
-              {t('meta.home')}
+        {isMobile ? null : (
+          <div className="top-bar__nav">
+            {user ? <AuthenticatedNav user={user} /> : null}
+            <NavLink to={createCompositeUrl(i18n, '/about')}>
+              {t('meta.about')}
             </NavLink>
-          )}
-          {user && (
-            <NavLink to={createCompositeUrl(i18n, '/my-quests/going')}>
-              {t('meta.my-quests')}
-            </NavLink>
-          )}
-          {user && (
-            <NavLink to={createCompositeUrl(i18n, '/create-new')}>
-              {t('meta.create-new')}
-            </NavLink>
-          )}
-          {user && (
-            <NavLink to={createCompositeUrl(i18n, `/user/${user.uid}`)}>
-              {t('meta.my-profile')}
-            </NavLink>
-          )}
-          <NavLink to={createCompositeUrl(i18n, '/about')}>
-            {t('meta.about')}
-          </NavLink>
-        </div>
+          </div>
+        )}
         <div className="top-bar__utils">
           <LanguageSwitcher />
           {user && <LogoutButton />}
         </div>
+        {isMobile ? (
+          <button
+            className="top-bar__floating-menu__trigger"
+            onClick={() => setOpenMenu(!openMenu)}
+          >
+            <IoMenu size={24} />
+          </button>
+        ) : null}
       </div>
+      {openMenu ? (
+        <div className="top-bar__floating-menu">
+          {user ? <AuthenticatedNav user={user} /> : null}
+          <NavLink to={createCompositeUrl(i18n, '/about')}>
+            {t('meta.about')}
+          </NavLink>
+        </div>
+      ) : null}
     </div>
   );
 }
