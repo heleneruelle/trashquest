540c6038d699fecaa3ada5c0871e6cf5a5a2c077
Hélène Ruelle <ruellehelene26@gmail.com>
2025-04-28 18:04:29 +0200

    wip add button link for updating user profile

--- app/pages/User.tsx
+++ app/pages/User.tsx
@@ -1,8 +1,12 @@
 import { useLoaderData } from '@remix-run/react';
 import { useTranslation } from 'react-i18next';
+import { LuPencil } from 'react-icons/lu';
 import UserType from '~/types/user';
 import QuestType from '~/types/quest';
 import QuestListItem from '~/components/display/QuestListItem';
+import ButtonLink from '~/components/inputs/ButtonLink';
+import createCompositeUrl from '~/utils/url/createCompositeUrl';
+import i18n from '~/i18n';
 
 interface LoaderData {
   success: boolean;
@@ -20,11 +24,15 @@ function User() {
   return (
     <div className="quests-container">
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        <h1>{username}</h1>
-        <img
+        {/*  <img
           src="/assets/default-avatar.webp"
           style={{ height: '75px', width: '75px' }}
-        />
+        /> */}
+        <h1>{username}</h1>
+        <ButtonLink target={createCompositeUrl(i18n, '/sign-up')}>
+          <LuPencil />
+          {t('user.cta.update')}
+        </ButtonLink>
       </div>
       <p>{location?.name}</p>
       <p>{t(`countries.${country}`)}</p>
