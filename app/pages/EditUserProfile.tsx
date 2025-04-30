b456ad5adf8ea78b09828463b86df9797aa9f9f1
Hélène Ruelle <ruellehelene26@gmail.com>
2025-04-30 21:50:42 +0200

    add styling and details

--- app/pages/EditUserProfile.tsx
+++ app/pages/EditUserProfile.tsx
@@ -1,5 +1,8 @@
 import { useLoaderData } from '@remix-run/react';
+import { useTranslation } from 'react-i18next';
 import EditUserProfileForm from '~/components/forms/EditUserProfileForm';
+import TwoColumnsLayout from '~/components/templates/TwoColumnsLayout';
+import ImageLayout from '~/components/templates/ImageLayout';
 import UserType from '~/types/user';
 
 interface LoaderData {
@@ -9,11 +12,17 @@ interface LoaderData {
 
 function EditUserProfile() {
   const { user } = useLoaderData<LoaderData>();
+  const { t } = useTranslation();
 
   return (
-    <div>
-      <EditUserProfileForm user={user} />
-    </div>
+    <TwoColumnsLayout>
+      <div className="edit-user-profile-form">
+        <h1>{t('user.edit.title')}</h1>
+        <h3>{t('user.edit.description')}</h3>
+        <EditUserProfileForm user={user} />
+      </div>
+      <ImageLayout />
+    </TwoColumnsLayout>
   );
 }
 
--- app/routes/$lang.user.edit.$id.tsx
+++ app/routes/$lang.user.edit.$id.tsx
@@ -5,7 +5,7 @@ import userLoader from '~/loaders/user';
 export const links: LinksFunction = () => {
   return [
     { rel: 'stylesheet', href: '/styles/form.css' },
-    { rel: 'stylesheet', href: '/styles/quests.css' },
+    { rel: 'stylesheet', href: '/styles/editUserProfile.css' },
   ];
 };
 
