08cb4f0f54199852c815a0a8e5cf38b79facb55f
Hélène Ruelle <ruellehelene26@gmail.com>
2025-05-20 13:47:40 +0200

    add loader data + edit form for EditQuest route page

--- app/pages/EditQuest.tsx
+++ app/pages/EditQuest.tsx
@@ -1,15 +1,26 @@
 import { useParams } from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react';
 import { useTranslation } from 'react-i18next';
 import ButtonLink from '~/components/inputs/ButtonLink';
+import EditQuestForm from '~/components/forms/EditQuestForm';
 import createCompositeUrl from '~/utils/url/createCompositeUrl';
 import i18n from '~/i18n';
+import QuestType from '~/types/quest';
+
+interface LoaderData {
+  success: boolean;
+  quest: QuestType;
+}
 
 function EditQuest() {
   const { id } = useParams();
   const { t } = useTranslation();
+  const { quest } = useLoaderData<LoaderData>();
+
   return (
-    <div>
-      Edit quest
+    <div className="quests-container">
+      <h1>{t('quest.edit.title')}</h1>
+      <EditQuestForm quest={quest} />
       <ButtonLink target={createCompositeUrl(i18n, `quest/${id}`)}>
         {t('quit')}
       </ButtonLink>
